806cc59ad88428c6a5f3cd02065772ba57853588
Lokesh <YOUR_GITHUB_EMAIL>
2026-09-07 23:04:52 -0700

    feat: implement employee urls and views

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import health_check
+from .views import health_check, employee_list, employee_detail
 
 urlpatterns = [
     path("health/", health_check, name="health-check"),
+    path("employees/", employee_list, name="employee-list"),
+    path("employees/<int:id>/", employee_detail, name="employee-detail"),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,8 +1,39 @@
 from django.http import JsonResponse
 
 
+employees = [
+    {
+        "id": 1,
+        "name": "Divya",
+        "department": "Backend",
+        "designation": "Python Developer"
+    },
+    {
+        "id": 2,
+        "name": "Rahul",
+        "department": "Frontend",
+        "designation": "React Developer"
+    }
+]
+
+
 def health_check(request):
     return JsonResponse({
         "status": "success",
         "message": "Employee Management Backend is running"
     })
+
+
+def employee_list(request):
+    return JsonResponse({
+        "employees": employees
+    })
+
+def employee_detail(request, id):
+    for employee in employees:
+        if employee["id"] == id:
+            return JsonResponse(employee)
+
+    return JsonResponse({
+        "error": "Employee not found"
+    }, status=404)
